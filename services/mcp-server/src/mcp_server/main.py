19f9d9fd0c60612a9b78511351b8095a8752a3c2
Yassine <yassinemakhlouf5@gmail.com>
2026-08-14 05:19:58 +0100

    SCRUM-64 Define and Expose LLM Tools

--- services/mcp-server/src/mcp_server/main.py
+++ services/mcp-server/src/mcp_server/main.py
@@ -10,6 +10,17 @@ mcp_server = FastMCP(
     port=services['mcp-server']['port'],
 )
 
+@mcp_server.tool(title='Modify File', description='Replaces the content of a file with new content.')
+async def modify_file(file_path: str, new_content: str):
+    url: str = f'{services['repository-storage-service']['endpoint']}/repositories/{file_path}'
+    update_file_content_request: UpdateFileContentRequest = UpdateFileContentRequest(
+        new_content=new_content
+    )
+    async with httpx.AsyncClient() as client:
+        response = await client.put(url, json=update_file_content_request)
+        response.raise_for_status()
+        return response.json()
+
 
 def main():
     mcp_server.run(transport='streamable-http')
