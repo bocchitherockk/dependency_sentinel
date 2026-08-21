b9879b3bac21b5a6df79105fe9f087eced5a41cf
Yassine <yassinemakhlouf5@gmail.com>
2026-08-21 05:15:45 +0100

    add logging for repository-scanner-service

--- services/repository_scanner_service/src/repository_scanner_service/__main__.py
+++ services/repository_scanner_service/src/repository_scanner_service/__main__.py
@@ -1,4 +1,8 @@
-from .main import main
+from common.logging.global_logger import get_global_logger
+logger = get_global_logger(__name__)
+logger.info('Starting the Repository Scanner Service...')
 
-if __name__ == "__main__":
-    main()
+from repository_scanner_service.main import main as repository_scanner_service_main
+
+def main():
+    repository_scanner_service_main()
