3ff185dca0acf23aed759c34d50c89604a674e4a
Yassine <yassinemakhlouf5@gmail.com>
2026-08-21 20:01:42 +0100

    add logging for security-intelligence-service

--- libs/common/src/common/schemas/ManifestFileUpdatePlan.py
+++ libs/common/src/common/schemas/ManifestFileUpdatePlan.py
@@ -1,6 +1,11 @@
+from logging import Logger
+
 from pydantic import BaseModel, Field
 
+from common.logging.global_logger import get_global_logger
 from common.schemas.DependencyUpdatePlan import DependencyUpdatePlan
+
+logger: Logger = get_global_logger(__name__)
 
 class ManifestFileUpdatePlan(BaseModel):
     manifest_file_path: str
@@ -8,16 +13,34 @@ class ManifestFileUpdatePlan(BaseModel):
     dev_dependencies_updates: list[DependencyUpdatePlan] = Field(default_factory=list)
 
     def remove_unnecessary_update_elements(self) -> None:
-        self.dependencies_updates = [
-            dependency_update_plan
-            for dependency_update_plan in self.dependencies_updates
-            if dependency_update_plan.current_version != dependency_update_plan.recommended_version
-        ]
-        self.dev_dependencies_updates = [
-            dev_dependency_update_plan
-            for dev_dependency_update_plan in self.dev_dependencies_updates
-            if dev_dependency_update_plan.current_version != dev_dependency_update_plan.recommended_version
-        ]
+        new_dependencies_updates: list[DependencyUpdatePlan] = []
+        for dependency_update_plan in self.dependencies_updates:
+            if dependency_update_plan.current_version != dependency_update_plan.recommended_version:
+                new_dependencies_updates.append(dependency_update_plan)
+            else:
+                logger.debug(f"Removing unnecessary dependency update for '{dependency_update_plan.dependency_name}' in manifest file '{self.manifest_file_path}' as current version '{dependency_update_plan.current_version}' is the same as recommended version '{dependency_update_plan.recommended_version}'.")
+
+        new_dev_dependencies_updates: list[DependencyUpdatePlan] = []
+        for dev_dependency_update_plan in self.dev_dependencies_updates:
+            if dev_dependency_update_plan.current_version != dev_dependency_update_plan.recommended_version:
+                new_dev_dependencies_updates.append(dev_dependency_update_plan)
+            else:
+                logger.debug(f"Removing unnecessary dev dependency update for '{dev_dependency_update_plan.dependency_name}' in manifest file '{self.manifest_file_path}' as current version '{dev_dependency_update_plan.current_version}' is the same as recommended version '{dev_dependency_update_plan.recommended_version}'.")
+
+        if len(new_dependencies_updates) != len(self.dependencies_updates) or len(new_dev_dependencies_updates) != len(self.dev_dependencies_updates):
+            logger.info(f"Removed unnecessary updates from ManifestFileUpdatePlan for manifest file '{self.manifest_file_path}'.")
+            logger.debug(f'Updated ManifestFileUpdatePlan details: {self}')
+        else:
+            logger.info(f"No unnecessary updates found in ManifestFileUpdatePlan for manifest file '{self.manifest_file_path}'.")
+
+        self.dependencies_updates = new_dependencies_updates
+        self.dev_dependencies_updates = new_dev_dependencies_updates
 
     def has_updates(self) -> bool:
-        return bool(self.dependencies_updates or self.dev_dependencies_updates)
+        result: bool = bool(self.dependencies_updates or self.dev_dependencies_updates)
+        if result:
+            logger.info(f"ManifestFileUpdatePlan for manifest file '{self.manifest_file_path}' has updates.")
+        else:
+            logger.info(f"ManifestFileUpdatePlan for manifest file '{self.manifest_file_path}' has no updates.")
+
+        return result
--- services/security_intelligence_service/src/security_intelligence_service/service.py
+++ services/security_intelligence_service/src/security_intelligence_service/service.py
@@ -1,8 +1,10 @@
 import asyncio
 from datetime import datetime, timezone
+from logging import Logger
 
 import httpx
 
+from common.logging.global_logger import get_global_logger
 from common.config import services
 from common.schemas.File import File
 from common.schemas.DependencyUpdateContext import DependencyUpdateContext
@@ -13,6 +15,8 @@ from common.schemas.CreateBranchRequest import CreateBranchRequest
 from common.schemas.UpdateManifestRequest import UpdateManifestRequest
 
 
+logger: Logger = get_global_logger(__name__)
+
 async def get_dependency_update_plan(dependency_update_context: DependencyUpdateContext) -> DependencyUpdatePlan:
     async with httpx.AsyncClient(timeout=None) as client:
         response = await client.post(
@@ -20,14 +24,20 @@ async def get_dependency_update_plan(dependency_update_context: DependencyUpdate
             json=dependency_update_context.model_dump(mode='json'),
         )
     response.raise_for_status()
-    return DependencyUpdatePlan(**response.json())
+    result: DependencyUpdatePlan = DependencyUpdatePlan(**response.json())
+    logger.info(f"DependencyUpdatePlan generated for dependency '{dependency_update_context.current_version_dependency_report.name}' with current version '{dependency_update_context.current_version_dependency_report.version}' in registry '{dependency_update_context.current_version_dependency_report.registry_name}'.")
+    logger.debug(f'DependencyUpdatePlan details: {result}')
+    return result
 
 async def get_dependencies_update_plan(dependencies_update_context: list[DependencyUpdateContext]) -> list[DependencyUpdatePlan]:
     tasks = [
         get_dependency_update_plan(context)
         for context in dependencies_update_context
     ]
-    return await asyncio.gather(*tasks)
+    result: list[DependencyUpdatePlan] = await asyncio.gather(*tasks)
+    logger.info(f"DependenciesUpdatePlan generated for {len(result)} dependencies.")
+    logger.debug(f'DependenciesUpdatePlan details: {result}')
+    return result
 
 async def get_manifest_file_update_plan(manifest_file_update_context: ManifestFileUpdateContext) -> ManifestFileUpdatePlan:
     dependencies_update_plans, dev_dependencies_update_plans = await asyncio.gather(
@@ -35,7 +45,7 @@ async def get_manifest_file_update_plan(manifest_file_update_context: ManifestFi
         get_dependencies_update_plan(manifest_file_update_context.dev_dependencies_update_context)
     )
 
-    return ManifestFileUpdatePlan(
+    result: ManifestFileUpdatePlan = ManifestFileUpdatePlan(
         manifest_file_path=manifest_file_update_context.manifest_file_path,
         dependencies_updates=[
             dependency_update_plan
@@ -48,6 +58,9 @@ async def get_manifest_file_update_plan(manifest_file_update_context: ManifestFi
             if dev_dependency_update_plan.current_version != dev_dependency_update_plan.recommended_version
         ],
     )
+    logger.info(f"ManifestFileUpdatePlan generated for manifest file '{manifest_file_update_context.manifest_file_path}'.")
+    logger.debug(f'ManifestFileUpdatePlan details: {result}')
+    return result
 
 async def create_git_branch(repository_name: str, branch_name: str) -> None:
     async with httpx.AsyncClient() as client:
@@ -59,7 +72,9 @@ async def create_git_branch(repository_name: str, branch_name: str) -> None:
             f"{services['repository-storage-service']['endpoint']}/create_branch",
             json=create_branch_request.model_dump(mode='json')
         )
-        response.raise_for_status()
+
+    response.raise_for_status()
+    logger.info(f"Git branch '{branch_name}' created for repository '{repository_name}'.")
 
 async def get_manifest_files(manifest_files_update_plans: list[ManifestFileUpdatePlan]) -> list[File]:
     async with httpx.AsyncClient() as client:
@@ -76,6 +91,8 @@ async def get_manifest_files(manifest_files_update_plans: list[ManifestFileUpdat
         response.raise_for_status()
         manifest_files.append(File(**response.json()))
 
+    logger.info(f"Retrieved manifest files for {len(manifest_files)} manifest files with content.")
+    logger.debug(f'Retrieved manifest files details: {manifest_files}')
     return manifest_files
 
 async def update_manifest_file(manifest_file: File, update_plan: ManifestFileUpdatePlan) -> str:
@@ -89,7 +106,10 @@ async def update_manifest_file(manifest_file: File, update_plan: ManifestFileUpd
             params={ 'model_name': 'gemini-3.5-flash-lite' },
             json=update_manifest_request.model_dump(mode='json'),
         )
+
     response.raise_for_status()
+    logger.info(f"Manifest file '{manifest_file.path}' updated with LLM service.")
+    logger.debug(f"Update reasoning for '{manifest_file.path}': {response.text}")
     return response.text
 
 async def analyze_update_context_and_update_manifests(
@@ -132,7 +152,10 @@ async def analyze_update_context_and_update_manifests(
     #   Pas de création de branche, pas de modification de fichiers.
     # ─────────────────────────────────────────────────────────────────
     if not manifest_files_update_plans:
+        logger.info(f"No updates needed for repository '{repository_name}'. No branch created, no files modified.")
         return None, None
+    logger.info(f"Updates needed for repository '{repository_name}'. Proceeding to create a new branch and update manifest files.")
+    logger.debug(f'ManifestFileUpdatePlans details: {manifest_files_update_plans}')
 
     # ─────────────────────────────────────────────────────────────────
     # Étape 3 : Créer une nouvelle branche Git
@@ -164,4 +187,6 @@ async def analyze_update_context_and_update_manifests(
         summary += f"Reasoning:\n{reasoning}\n"
         summary += "───────────────────────────────────────────\n"
 
+    logger.info(f"Manifest files updated for repository '{repository_name}' on branch '{branch_name}'.")
+    logger.debug(f'Update summary as PR body for repository {repository_name}:\n{summary}')
     return branch_name, summary
